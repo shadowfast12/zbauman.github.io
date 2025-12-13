2dfcb66b3ba8aa8853999c03707145d56a941c52
zach <zach.bauman12@gmail.com>
2025-12-13 15:43:34 -0600

    Updates

--- src/App.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import PortfolioLayout from "./components/layout/PortfolioLayout.tsx";
-
-export default function App() {
-    return <PortfolioLayout />;
-}
--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  base: "https://zbauman.github.io", // <- replace with your GitHub repo name (trailing slash)
-  plugins: [react()]
-});
